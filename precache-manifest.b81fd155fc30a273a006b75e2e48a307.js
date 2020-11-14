79717a3d5a3257945117b4039af0d70fd916df28
Jin <moeprprpr@gmail.com>
2020-11-14 12:07:39 +0800

    Site updated: 2020-11-14 12:07:39

---
+++ precache-manifest.b81fd155fc30a273a006b75e2e48a307.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f288817d0e33c016b05f",
+    "url": "/css/app.f301cdaa.css"
+  },
+  {
+    "revision": "ad1f90c8a77d4335a42f",
+    "url": "/css/archive.aa025432.css"
+  },
+  {
+    "revision": "6325712d7c9094239d01",
+    "url": "/css/archive~single.a73dc46a.css"
+  },
+  {
+    "revision": "86bd91a886345cd2c79a5083d743e6a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f288817d0e33c016b05f",
+    "url": "/js/app.789e6258.js"
+  },
+  {
+    "revision": "ad1f90c8a77d4335a42f",
+    "url": "/js/archive.75280a7f.js"
+  },
+  {
+    "revision": "b5a78ddda001c63ebb08",
+    "url": "/js/archives.8af14e01.js"
+  },
+  {
+    "revision": "6325712d7c9094239d01",
+    "url": "/js/archive~single.b89ab100.js"
+  },
+  {
+    "revision": "252011455736d707a963",
+    "url": "/js/chunk-vendors.adf8da55.js"
+  },
+  {
+    "revision": "d8ff3c41a1d28ed89bea",
+    "url": "/js/home.267c3f71.js"
+  },
+  {
+    "revision": "5eaf32e591f80b222eb7",
+    "url": "/js/page404.6f4f2b86.js"
+  },
+  {
+    "revision": "3a0fec103d53fae8d441",
+    "url": "/js/single.6265c947.js"
+  },
+  {
+    "revision": "9496fd0e9a26f0f14a60",
+    "url": "/js/term.f8c529aa.js"
+  },
+  {
+    "revision": "4bccd950b103ed00937b11590e029708",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,99 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
+importScripts("https://cdn.jsdelivr.net/npm/workbox-sw", "/precache-manifest.b81fd155fc30a273a006b75e2e48a307.js");
+
+/*
+ * @Author: Jin
+ * @Date: 2020-09-02 16:57:52
+ * @LastEditors: Jin
+ * @LastEditTime: 2020-09-02 22:20:04
+ * @FilePath: /Stack-Vue-Hexo/src/service-worker.js
  */
-
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/precache-manifest.f571e011f102b31ef67f04ccae39c988.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "thi-lite"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+workbox.core.setCacheNameDetails({
+    prefix: 'Stack',
+    suffix: 'v1.0.0'
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerRoute(
+    /.*\.css$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'css-cache'
+    })
+);
+workbox.routing.registerRoute(
+    /.*\.js$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'js-cache'
+    })
+);
+workbox.routing.registerRoute(
+    /\.(?:eot|ttf|woff|woff2)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'fonts-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+            })
+        ]
+    })
+);
+workbox.routing.registerRoute(
+    /^https:\/\/fonts\.aim\.moe\//,
+    new workbox.strategies.CacheFirst({
+        cacheName: 'google-fonts-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            })
+        ]
+    })
+);
+workbox.routing.registerRoute(
+    /^https:\/\/static\.misaka\.xin\/.*/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'cdn-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+            })
+        ],
+        fetchOptions: {
+            credentials: 'include'
+        }
+    })
+);
+workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg|webp)$/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'images-cache',
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60
+            })
+        ]
+    })
+);
+workbox.routing.registerRoute(
+    /\/api\/.*/,
+    workbox.strategies.networkFirst({
+        cacheName: 'api-cache'
+    })
+);
+workbox.routing.registerRoute(
+    /\/comments\/.*/,
+    workbox.strategies.networkFirst({
+        cacheName: 'comment-api-cache'
+    })
+);
+workbox.routing.registerRoute(
+    /\/.*/,
+    workbox.strategies.staleWhileRevalidate({
+        cacheName: 'other-cache',
+        fetchOptions: {
+            credentials: 'include'
+        }
+    })
+);
+
