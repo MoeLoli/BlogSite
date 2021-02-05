36f23433b4abcfdbb567d1b58ea1f085922caf3a
Jin <10751888+MoeLoli@users.noreply.github.com>
2021-02-05 13:55:32 +0800

    Site updated: 2021-02-05 13:55:32

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ importScripts("https://cdn.jsdelivr.net/npm/workbox-sw/build/workbox-sw.js", "/p
  * @Author: Jin
  * @Date: 2020-09-02 16:57:52
  * @LastEditors: Jin
- * @LastEditTime: 2021-02-05 13:41:00
+ * @LastEditTime: 2021-02-05 13:54:36
  * @FilePath: /hexo-theme-stack/src/service-worker.js
  */
 workbox.core.setCacheNameDetails({
@@ -75,12 +75,12 @@ workbox.routing.registerRoute(
         ]
     })
 );
-// workbox.routing.registerRoute(
-//    /\/api\/.*/,
-//    new workbox.strategies.NetworkFirst({
-//        cacheName: 'api-cache'
-//    })
-// );
+workbox.routing.registerRoute(
+    /\/api\/.*/,
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'api-cache'
+    })
+);
 workbox.routing.registerRoute(
     /\/.*/,
     new workbox.strategies.StaleWhileRevalidate({
